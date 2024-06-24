ba8979b7a0eb7e81a515b3ba1e305949d00d588e
shadan <shadan.hussain.2004@gmail.com>
2024-06-24 16:11:07 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -1,39 +1,48 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const mongoose=require("mongoose");
 
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
-const adminRouter = require("./routes/admin");
+const adminRouter=require("./routes/admin");
 const userRouter = require("./routes/user");
 const applicationRouter = require("./routes/application");
-const notificationRouter = require("./routes/notification");
-const teamRouter = require("./routes/teams");
-const messageRouter = require("./routes/message");
+const notifcationRouter = require("./routes/notification");
+const teamRouter=require("./routes/teams");
+const messageRouter =require("./routes/message");
 require('dotenv').config();
+const cors = require('cors');
 
-// INIT
-const PORT = process.env.PORT || 3000;
 
-const app = express();
+const PORT = process.env.PORT ||  3000; 
+
+const app = express(); 
 const dbUrl = process.env.DATABASE_URL;
 
+
+app.use(cors());
 app.use(express.json());
 app.use(authRouter);
 app.use(profileRouter);
 app.use(adminRouter);
 app.use(applicationRouter);
-app.use(userRouter);
-app.use(notificationRouter);
+app.use(userRouter); 
+app.use(notifcationRouter);
 app.use(teamRouter);
 app.use(messageRouter);
 
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the API');
+});
 // Connections
-mongoose.connect(dbUrl).then(() => {
+ mongoose.connect(dbUrl).then(()=>{
     console.log("Successfully connected");
-}).catch((e) => {
-    console.log(e);
+ })
+ .catch((e)=>{console.log(e);})
+
+
+app.listen(PORT,"0.0.0.0", () => {
+  console.log(`connected at port ${PORT} hello`);
 });
 
-app.listen(PORT, "0.0.0.0", () => {
-    console.log(`connected at port ${PORT}`);
-});
+//127.0.0.1
